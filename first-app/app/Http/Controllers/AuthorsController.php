1dae649ae96db30c61ba45e7c6ca96a8cc775d70
gychem <gychem.baert@hotmail.com>
2022-08-12 12:28:36 +0200

    Make code structure pretty

--- first-app/app/Http/Controllers/AuthorsController.php
+++ first-app/app/Http/Controllers/AuthorsController.php
@@ -11,20 +11,7 @@ class AuthorsController extends Controller
 {
     public function index()
     {
-      //  $allAuthors = [];
-
-        // foreach (Article::all() as $article) { 
-        
-        //     $object = ['name' => $article->author->name, 'id' => $article->author->id];
-
-        //     array_push($allAuthors, $object);
-        // }
-        
-       // $authors = ($allAuthors);
-
         $authors = Author::all();
-
-       // dd($authors);
 
         return view('admin.articles.authors')->with('authors', $authors);
     }
@@ -34,13 +21,11 @@ class AuthorsController extends Controller
         $articles = [];
 
         foreach (Article::all() as $article) { 
-        
+
             if($article->author->username == $author->username) {
-
-               // $object = ['name' => $article->author->name, 'id' => $article->author->id];
-
                 array_push($articles, $article);
             }
+            
         }
 
         return view('admin.articles.author')->with('articles', $articles);
