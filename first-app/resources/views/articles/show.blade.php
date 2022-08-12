f68df09b76898d401aca39d7b0977f5b5a44b8f3
gychem <gychem.baert@hotmail.com>
2022-08-12 23:05:54 +0200

    Hide comment form when not logged in, improve styling

--- first-app/resources/views/articles/show.blade.php
+++ first-app/resources/views/articles/show.blade.php
@@ -22,31 +22,37 @@
         </article>
     </div>
 
-    <div class="w-full rounded shadow-md mt-4 bg-white pl-6 pr-6 pt-2 pb-2 dark:bg-gray-700">
-        <form method="POST" action="/news/{{ $article->slug }}/comment">
-            @csrf
-            <header class="flex items-center">
-                <div class="flex-shrink-0"> 
-                    <img class="rounded shadow-md mt-1" src="https://i.pravatar.cc/60?u={{ auth()->user()->id }}" width="60" height="60" alt="">
+    @auth
+        <div class="w-full rounded shadow-md mt-4 bg-white pl-6 pr-6 pt-2 pb-2  dark:bg-gray-700">
+            <form method="POST" action="/news/{{ $article->slug }}/comment">
+                @csrf
+                <header class="flex items-center">
+                    <div class="flex-shrink-0"> 
+                        <img class="rounded shadow-md mt-1" src="https://i.pravatar.cc/60?u={{ auth()->user()->id }}" width="60" height="60" alt="">
+                    </div>
+                    <h2 class="ml-4 w-11/12 dark:text-gray-300">Comment</h2>
+                </header>
+
+                <div class="mt-4">
+                    <textarea name="body" id="body" class="border-white shadow w-full rounded py-2 px-3 text-gray-700 disable-outline shadow-outline" cols="30" rows="5" placeholder="Write your comment in this textarea"></textarea>
+                    @error('body')
+                        <div class="text-red-500 text-xs italic">{{ $message }}</div>
+                    @enderror('body') 
                 </div>
-                <h2 class="ml-4 w-11/12 dark:text-gray-300">Comment</h2>
-            </header>
 
-            <div class="mt-4">
-                <textarea name="body" id="body" class="border-white shadow w-full rounded py-2 px-3 text-gray-700 disable-outline shadow-outline" cols="30" rows="5" placeholder="Write your comment in this textarea"></textarea>
-                @error('body')
-                    <div class="text-red-500 text-xs italic">{{ $message }}</div>
-                @enderror('body') 
-            </div>
+                <div class="flex items-center justify-end mt-2 mr-6">
+                    <x-button>
+                        Post
+                    </x-button>
+                </div>
+            </form>
+        </div>
+    @else 
+        <div class="w-full rounded flex justify-center shadow-md mt-4 bg-white pl-6 pr-6 pt-2 pb-2 dark:text-slate-400  dark:bg-gray-700">
+            <a class="mr-1 text-gray-500 font-semibold dark:text-white hover:text-gray-400" href="{{route('login')}}">Log-in </a>  to write a comment.
+        </div>
+    @endauth 
 
-            <div class="flex items-center justify-end mt-2 mr-6">
-                <x-button>
-                    Post
-                </x-button>
-            </div>
-        </form>
-    </div>
-        
     @foreach($article->comments as $comment)
         <x-article_comment :comment="$comment" />
     @endforeach
