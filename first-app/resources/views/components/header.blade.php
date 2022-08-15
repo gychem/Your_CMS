b5ebc5f82e18d4aa6b45178e9b607e9568093814
gychem <gychem.baert@hotmail.com>
2022-08-15 23:38:29 +0200

    Update styling

--- first-app/resources/views/components/header.blade.php
+++ first-app/resources/views/components/header.blade.php
@@ -16,7 +16,7 @@
                         </li>
                     @endforeach
                     <li>
-                        <a href="{{route('news')}}" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">News</a>
+                        <a href="{{route('news')}}" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">News</a>
                     </li>
                     <li>
                         <a href="{{route('contact')}}" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
