f3b6112316faccd0c977d39f0c17d51694133caa
gychem <gychem.baert@hotmail.com>
2022-08-15 15:15:18 +0200

    Add button link (a href) component

---
+++ first-app/resources/views/components/button-link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes }} class="flex gap-2 text-gray-800 bg-gray-50 hover:bg-gray-100 py-2 px-4 rounded shadow-md dark:text-gray-300 dark:bg-gray-800 dark:hover:bg-gray-600" type="submit">
+    {{ $slot }}
+</a>
