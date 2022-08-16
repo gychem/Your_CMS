8c647daa67a0885532b930678715191cdcda9d40
Gychem Baert <gychem.baert@hotmail.com>
2022-08-16 14:04:48 +0200

    Update styling

--- first-app/resources/views/articles/overview.blade.php
+++ first-app/resources/views/articles/overview.blade.php
@@ -10,7 +10,7 @@
         </form>
 
         <div class="container w-full mt-3 mx-auto space-y-6 sm:space-y-12">
-            <a rel="noopener noreferrer" href="/news/{{ $data['firstArticle']->slug }}" class="block bg-white rounded shadow-md max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
+            <a rel="noopener noreferrer" href="/news/{{ $data['firstArticle']->slug }}" class="block bg-white hover:bg-gray-100 rounded shadow-md max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-900">
                 <img src="{{ $data['firstArticle']->image }}" alt="" class="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500">
                 <div class="p-6 space-y-2 lg:col-span-5">
                     <h3 class="text-2xl font-semibold sm:text-4xl dark:text-white">{{ $data['firstArticle']->title }}</h3>
@@ -18,7 +18,7 @@
                     <p  class="dark:text-gray-400">{{ $data['firstArticle']->excerpt }}.</p>
                 </div>
             </a>
-            <div class="grid justify-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <div class="grid justify-center grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3" style="margin-top:10px">
              
                 @foreach ($data['articles'] as $article) 
                     <a rel="noopener noreferrer" href="/news/{{ $article->slug }}" class="bg-white hover:bg-gray-100 rounded shadow-md  w-full mx-auto group hover:no-underline focus:no-underline dark:bg-gray-700 dark:hover:bg-gray-800">
